8f9a2663c926c44bb129060231a69ff47324f7fe
meve404 <procontact325@gmail.com>
2024-01-25 21:51:34 -0500

    celery tasks

---
+++ cfehome/movies/tasks.py
@@ -0,0 +1,18 @@
+import random
+from celery import shared_task
+
+@shared_task
+def add(x, y):
+    # Celery recognizes this as the `movies.tasks.add` task
+    return x + y
+
+@shared_task(name="multiply_two_numbers")
+def mul(x, y):
+    # Celery recognizes this as the `multiple_two_numbers` task
+    total = x * (y * random.randint(3, 100))
+    return total
+
+@shared_task(name="sum_list_numbers")
+def xsum(numbers):
+    # Celery recognizes this as the `sum_list_numbers` task
+    return sum(numbers)
