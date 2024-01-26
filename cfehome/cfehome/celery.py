040c74359b0439fa0f1223687386be1e4c737823
meve404 <procontact325@gmail.com>
2024-01-25 21:09:29 -0500

    celery config file

---
+++ cfehome/cfehome/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+from decouple import config
+
+# set the default Django settings module for the 'celery' program.
+# this is also used in manage.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
+
+app = Celery('cfehome')
